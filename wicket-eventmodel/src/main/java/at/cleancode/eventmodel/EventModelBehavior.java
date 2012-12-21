c30addb287b7689e2bb72f637c981fd59dbfdb0c
Christoph Leiter <cl@samera.at>
2012-12-21 13:06:23 +0100

    Add logging in EventModelBehavior

--- wicket-eventmodel/src/main/java/at/cleancode/eventmodel/EventModelBehavior.java
+++ wicket-eventmodel/src/main/java/at/cleancode/eventmodel/EventModelBehavior.java
@@ -5,9 +5,13 @@ import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.event.IEvent;
 import org.apache.wicket.request.cycle.RequestCycle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SuppressWarnings("rawtypes")
 public class EventModelBehavior extends Behavior {
+
+    private static final Logger log = LoggerFactory.getLogger(EventModelBehavior.class);
 
     private final Class<? extends EventModel> modelClass;
 
@@ -42,6 +46,10 @@ public class EventModelBehavior extends Behavior {
         if (oldValue == null || !oldValue.equals(currentValue)) {
             oldValue = currentValue;
             RequestCycle.get().find(AjaxRequestTarget.class).add(component);
+            log.debug("Updating component '{}': Old value: '{}', new value: '{}'", component.getPath(), oldValue,
+                    currentValue);
+        } else {
+            log.debug("The value for component '{}' didn't change ('{}')", component.getPath(), currentValue);
         }
     }
 
